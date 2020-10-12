c0324ae13aa7bd5d9383023cde3ed1a0a5fbec84
EvgSumina <57412601+EvgSumina@users.noreply.github.com>
2020-10-13 02:08:20 +0300

    Add files via upload

--- 01/allocator.h
+++ 01/allocator.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 
 using namespace std;
@@ -10,78 +11,9 @@ class Allocator
 	bool flag;
 
 public:
-	Allocator()
-	{
-		allocator = nullptr;
-		flag = true;
-		offset = 0;
-	}
-
-	void makeAllocator(size_t maxSize)
-	{
-		try
-		{
-			if (flag)
-			{
-				allocator = new char[maxSize];
-				max_size = maxSize;
-				flag = false;
-			}
-			else
-			{
-				throw "I have already allocate\n";
-			}
-		}
-
-		catch (char const* str)
-		{
-			cout << str;
-		}
-	}
-
-	char* alloc(size_t size)
-	{
-		try
-		{
-			if (flag)
-			{
-				throw "Allocate at first\n";
-			}
-			if ((size + offset) <= max_size)
-			{
-				offset += size;
-				return (allocator + offset - size);
-			}
-			return nullptr;
-		}
-
-		catch (char const* str)
-		{
-			cout << str;
-		}
-	}
-
-    void reset()
-	{
-		try
-		{
-			if (flag)
-			{
-				throw "Allocate,then reset\n";
-			}
-			offset = 0;
-		}
-
-		catch (char const* str)
-		{
-			cout << str;
-		}
-	};
-
-	~Allocator()
-	{
-		if (allocator != nullptr)
-			delete []allocator;
-	}
+	Allocator();
+	void makeAllocator(size_t maxSize);
+	char* alloc(size_t size);
+	void reset();
+	~Allocator();
 };
-
