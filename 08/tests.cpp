7ba3028b58342ef57cdbde6a052ef37aee9fdd0a
EvgSumina <57412601+EvgSumina@users.noreply.github.com>
2020-12-02 22:21:30 +0300

    Add files via upload

---
+++ 08/tests.cpp
@@ -0,0 +1,20 @@
+#include "thread_pool.hpp"
+#include <iostream>
+
+struct A {};
+
+void foo(const A&) {}
+
+int main() 
+{
+	ThreadPool pool(8);
+
+	auto task1 = pool.exec(foo, A());
+	task1.get();
+
+	auto task2 = pool.exec([]() { return 1; });
+	task2.get();
+	
+	std::cout << "Success" << std::endl;
+	return 0;
+}
